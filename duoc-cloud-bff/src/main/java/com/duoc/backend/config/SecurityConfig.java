0a5a09bd6ad537b1d7e7293b52556a2cc78ba43a
simonky <simonasb19@gmail.com>
2025-02-23 10:23:53 -0300

    fix security config

--- duoc-cloud-bff/src/main/java/com/duoc/backend/config/SecurityConfig.java
+++ duoc-cloud-bff/src/main/java/com/duoc/backend/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(authorize -> 
 				authorize
 					.anyRequest()
-					.permitAll()
+					.authenticated()
 			)
 			.oauth2ResourceServer((oauth2) -> 
 				oauth2.jwt(Customizer.withDefaults())
